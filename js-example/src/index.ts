20874efa960f9726f101f9390411129fd8bb7b7d
Jeret <1600012705@pku.edu.cn>
2022-06-30 21:54:17 +0800

    fix

--- js-example/src/index.ts
+++ js-example/src/index.ts
@@ -2,6 +2,7 @@ import {
     ApplicationContext,
     FacemojiAPI,
     FaceTracker,
+    FaceTrackerResult,
     FaceTrackerResultDeserializer,
     FaceTrackerResultSerializer,
     FPS,
@@ -11,6 +12,9 @@ import {
     ResourceFileSystem,
     Vec2,
 } from '@0xalter/mocap4face'
+
+import { DebugoutOptions, Debugout } from 'debugout.js';
+
 import './styles/main.scss'
 
 Logger.logLevel = LogLevel.Info // Set LogLevel.Debug to increase logging verbosity when debugging
@@ -21,21 +25,70 @@ const contentElement = document.getElementById('blendshapes')!
 const statusElement = document.getElementById('status')!
 const fpsElement = document.getElementById('fps')
 const fallbackVideo = videoElement.currentSrc
+var tmpResult: FaceTrackerResult
+var count: number = 2;
+var line: number = 0;
+var videoName: string = `${count}.mp4`;
+
+
+
+var names:string[] = [
+    "browInnerUp_L",
+    "browInnerUp_R",
+    //(browInnerUp_L + browInnerUp_R) / 2
+    "browDown_L",
+    "eyeBlink_L",
+    "eyeSquint_L",
+    "eyeWide_L",
+    "eyeLookUp_L",
+    "eyeLookOut_L",
+    "eyeLookIn_L",
+    "eyeLookDown_L",
+    "noseSneer_L",
+    "mouthUpperUp_L",
+    "mouthSmile_L",
+    "mouthLeft",
+    "mouthFrown_L",
+    "mouthLowerDown_L",
+    "jawLeft",
+    "cheekPuff",
+    "mouthShrugUpper",
+    "mouthFunnel",
+    "mouthRollLower",
+    "jawOpen",
+    "tongueOut",
+    "mouthPucker",
+    "mouthRollUpper",
+    "jawRight",
+    "mouthLowerDown_R",
+    "mouthFrown_R",
+    "mouthRight",
+    "mouthSmile_R",
+    "mouthUpperUp_R",
+    "noseSneer_R",
+    "eyeLookDown_R",
+    "eyeLookIn_R",
+    "eyeLookOut_R",
+    "eyeLookUp_R",
+    "eyeWide_R",
+    "eyeSquint_R",
+    "eyeBlink_R",
+    "browDown_R",
+    "browOuterUp_R"]
 
 function startTracking() {
     const faceRectangleElement = document.getElementById('rectangle')
     const blendshapeSliders = new Map<String, HTMLElement>()
     const context = new ApplicationContext(window.location.href) // Set a different URL here if you host application resources elsewhere
     const fs = new ResourceFileSystem(context)
-    const fps = new FPS(1)
-
+    
     // uncomment for de/serialization example bellow
     // const serializer = FaceTrackerResultSerializer.create()
     // const deserializer = FaceTrackerResultDeserializer.create(serializer.serializationFormat)
 
     // Initialize the API and activate API key
     // Note that without an API key the SDK works only for a short period of time
-    FacemojiAPI.initialize('<YOUR KEY HERE>', context).then((activated) => {
+    FacemojiAPI.initialize('caxiyijtxyappzlg2nklhgxemqmidtqicakjyme6vnyms5cp34im4oa', context).then((activated) => {
         if (activated) {
             console.info('API successfully activated')
         } else {
@@ -44,6 +97,36 @@ function startTracking() {
     })
 
     const webcamAvailable = checkWebcamAvailable()
+    videoElement.oncanplay = ()=> {
+        bugout.log(videoName)
+        videoElement.playbackRate = 0.2
+
+        if (count == 2){
+            videoElement.currentTime = 970.92
+        }
+        videoElement.play()
+        videoElement.requestVideoFrameCallback(track)
+    }
+
+    function videoEnd(){
+        console.log("end")
+    
+        count++
+        if (count == 7){
+            bugout.downloadLog()
+            //bugout.clear()
+            return
+        }
+        videoName = `${count}.mp4`
+        videoElement.setAttribute('src', videoName)
+        videoElement.load()
+        
+    
+    }
+
+    const bugout = new Debugout()
+    bugout.autoTrim = false
+    bugout.realTimeLoggingOn = true
 
     // Initialize
     const asyncTracker = FaceTracker.createVideoTracker(fs)
@@ -63,115 +146,105 @@ function startTracking() {
                 contentElement.appendChild(li)
                 blendshapeSliders.set(blendshape, div)
             }
-
-            requestAnimationFrame(track)
+            statusElement.hidden = true
+            videoElement.load()
             return tracker
         })
         .logError('Could not start tracking')
+
 
     // Show webcam button after tracker loads and when webcam is available
     Promise.all([webcamAvailable, asyncTracker.promise()]).then(() => {
         webcamOverlay.classList.remove('hidden')
     })
-
+ 
     /**
      * Shows or hides rectangle around the detected face
      * @param show whether to show the face rectangle
      */
-    function setFaceRectangleVisible(show: boolean) {
-        if (faceRectangleElement !== null) {
-            faceRectangleElement.style.display = show ? 'block' : 'none'
-        }
-    }
 
     /**
      * Performs face tracking, called every animation frame.
      */
+     function setFaceRectangleVisible(show: boolean) {
+        if (faceRectangleElement !== null) {
+            faceRectangleElement.style.display = show ? 'block' : 'none'
+        }
+    }
     function track() {
-        requestAnimationFrame(track)
         const tracker = asyncTracker.currentValue
 
         // Track only when everything is fully loaded and video is running
         if (!tracker || videoElement === null || contentElement === null) {
-            setFaceRectangleVisible(false)
+            console.log("oh no")
+            videoElement.requestVideoFrameCallback(track)
             return
         }
 
-        if (videoElement.paused || document.hidden) {
-            statusElement.hidden = false
-            return
-        }
-
-        statusElement.hidden = true
-
+        videoElement.pause()
+        const timeStamp = videoElement.currentTime
         // Face tracking
-        const lastResult = tracker.track(videoElement)
-        if (lastResult == null) {
+        var lastResult = tracker.track(videoElement)
+
+        
+        if (lastResult != null){
+
+            var result: number[] = new Array(40)
+            videoElement.className = videoResolutionClass(lastResult.inputImageSize)
+            result[0] = ((lastResult.blendshapes.get(names[0]) as number) + 
+                        (lastResult.blendshapes.get(names[1]) as number) ) / 2.0
+                
+            for (var j = 2; j < names.length; j++){
+                result[j -1] = lastResult.blendshapes.get(names[j]) as number
+            }
+            bugout.log(timeStamp)
+            bugout.log(result)
+            line ++
+            if (line == 500){
+                bugout.downloadLog()
+                bugout.clear()
+                line = 0
+            }
+
+
+            if (faceRectangleElement !== null) {
+                // Align overlay parent size with video size
+                const parent = faceRectangleElement.parentElement
+                if (parent !== null) {
+                    parent.style.left = videoElement.offsetLeft + 'px'
+                    parent.style.top = videoElement.offsetTop + 'px'
+                    parent.style.width = videoElement.clientWidth + 'px'
+                    parent.style.height = videoElement.clientHeight + 'px'
+                }
+
+                // Convert face rectangle from tracker coordinates to HTML coordinates
+                const rect = lastResult.faceRectangle
+                    .flipY(lastResult.inputImageSize.y)
+                    .normalizeBy(lastResult.inputImageSize)
+                    .scale(videoElement.clientWidth, videoElement.clientHeight)
+                    .scaleAroundCenter(0.8, 0.8) // mocap4face uses a wider rect for better detection, a smaller one is more pleasing to the eye though
+                faceRectangleElement.style.position = 'relative'
+                faceRectangleElement.style.left = rect.x.toString() + 'px'
+                faceRectangleElement.style.top = rect.y.toString() + 'px'
+                faceRectangleElement.style.width = rect.width.toString() + 'px'
+                faceRectangleElement.style.height = rect.height.toString() + 'px'
+
+                // At this point the tracker always detected some face but it might be a low confidence one.
+                // hasFace() checks whether the tracker is confident enough about the detection.
+                // You can also read the confidence value itself by checking lastResult.confidence
+                setFaceRectangleVisible(lastResult.hasFace())
+            }
+        }   else{
             setFaceRectangleVisible(false)
-            return // No face found or video frame could not be processed
         }
-
-        // Serialize/deserialize tracking result for e.g. sending over WebRTC, use TrackerResultAvatarController for that
-        // if (lastResult) {
-        //     // send this over network or save to file
-        //     // DON'T FORGET to send serializer.serializationFormat as well, it's necessary for deserializer instantiation
-        //     const serialized = serializer.serialize(lastResult)
-
-        //     // see deserializer creation above, serializationFormat is required
-        //     const deserialized = deserializer.deserialize(serialized)
-
-        //     const result = deserialized.trackerResult
-        //     console.log(result)
-        // }
-
-        // Update UI
-        for (const [name, value] of lastResult.blendshapes) {
-            updateBlendshapeValue(name, value)
+        
+        videoElement.currentTime += 0.05
+        if (videoElement.ended){
+            videoEnd()
+            return
         }
-
-        const rotationBlendshapes = faceRotationToBlendshapes(lastResult.rotationQuaternion)
-        for (const [name, value] of rotationBlendshapes) {
-            updateBlendshapeValue(name, value)
-        }
-
-        videoElement.className = videoResolutionClass(lastResult.inputImageSize)
-
-        // Update face reactangle overlay
-        if (faceRectangleElement !== null) {
-            // Align overlay parent size with video size
-            const parent = faceRectangleElement.parentElement
-            if (parent !== null) {
-                parent.style.left = videoElement.offsetLeft + 'px'
-                parent.style.top = videoElement.offsetTop + 'px'
-                parent.style.width = videoElement.clientWidth + 'px'
-                parent.style.height = videoElement.clientHeight + 'px'
-            }
-
-            // Convert face rectangle from tracker coordinates to HTML coordinates
-            const rect = lastResult.faceRectangle
-                .flipY(lastResult.inputImageSize.y)
-                .normalizeBy(lastResult.inputImageSize)
-                .scale(videoElement.clientWidth, videoElement.clientHeight)
-                .scaleAroundCenter(0.8, 0.8) // mocap4face uses a wider rect for better detection, a smaller one is more pleasing to the eye though
-            faceRectangleElement.style.position = 'relative'
-            faceRectangleElement.style.left = rect.x.toString() + 'px'
-            faceRectangleElement.style.top = rect.y.toString() + 'px'
-            faceRectangleElement.style.width = rect.width.toString() + 'px'
-            faceRectangleElement.style.height = rect.height.toString() + 'px'
-
-            // At this point the tracker always detected some face but it might be a low confidence one.
-            // hasFace() checks whether the tracker is confident enough about the detection.
-            // You can also read the confidence value itself by checking lastResult.confidence
-            setFaceRectangleVisible(lastResult.hasFace())
-        }
-
-        // Update FPS counter
-        fps.tick((currentFps) => {
-            if (fpsElement !== null) {
-                fpsElement.parentElement!.className = ''
-                fpsElement.innerText = currentFps.toFixed(0)
-            }
-        })
+        videoElement.play()
+        videoElement.requestVideoFrameCallback(track)
     }
 
     /**
@@ -261,12 +334,15 @@ function videoResolutionClass(resolution: Vec2): string {
         ['3_4', 3 / 4],
     ]
     const currentRatio = resolution.x / resolution.y
+    var mm = 1000
+    var choose: string = '123';
     for (const clsAndRatio of knownRatios) {
-        if (Math.abs(clsAndRatio[1] - currentRatio) <= 0.01) {
-            return 'ratio_' + clsAndRatio[0]
+        if (Math.abs(clsAndRatio[1] - currentRatio) <= mm) {
+            mm = Math.abs(clsAndRatio[1] - currentRatio)
+            choose = 'ratio_' + clsAndRatio[0]
         }
     }
-    return 'ratio_unknown'
+    return choose
 }
 
 /**
@@ -316,10 +392,10 @@ webcamButton.addEventListener('click', () => {
 
 // Do not eat resources when our tab is in the background
 window.onfocus = () => {
-    videoElement.play()
+    //videoElement.play()
 }
 window.onblur = () => {
-    videoElement.pause()
+    //videoElement.pause()
 }
 
 // Start tracking
